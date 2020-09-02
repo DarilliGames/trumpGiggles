1c590ef5ab93e6bf429ec6fa4fd079d9eef3ea6e
Stephen Moody <darilligames@gmail.com>
2020-09-02 10:55:57 +0000

    updates

--- themain.js
+++ themain.js
@@ -1,28 +1,18 @@
 function run(){
-    // var something  = document.getElementsByClassName("page-header__title")[0];
-    // something.innerHTML = "President Fart";
 
     var all_tags= ["H1", "H2", "P", "LI", "SPAN"];
     for (var i = 0; i<all_tags.length; i++){
         var elements_by_tag = document.getElementsByTagName(all_tags[i]);
-        console.log(i);
         for (var j = 0; j < elements_by_tag.length; j++){
-            console.log(j)
-            
             var the_ele = elements_by_tag[j];
             var the_ele_str = the_ele.innerHTML;
-            console.log(the_ele_str);
             the_ele_str = the_ele_str.replace("Donald Trump", "President Farts");
             the_ele_str = the_ele_str.replace("Donald J Trump", "President Farts");
             the_ele_str = the_ele_str.replace("Donald J. Trump", "President Farts");
-            console.log(the_ele_str);
-
             the_ele.innerHTML = the_ele_str;
 
         }
     }
 }
-
-
 
 run();
